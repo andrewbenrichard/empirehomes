9e8d5f64af7eeceaca64780d870159f092e246fd
Andrew Ben Richard <andrewbenrichard@gmail.com>
2020-07-14 11:36:15 +0100

    ex

--- resources/views/layouts/front_extras.blade.php
+++ resources/views/layouts/front_extras.blade.php
@@ -1,27 +1,27 @@
-<script src="js/front_app.js"></script>
+<script src="public/js/front_app.js"></script>
 	<!--jQuery Plugin -->
-	<script src="assets/front/js/jquery-3.4.1.min.js"></script>
+	<script src="public/assets/front/js/jquery-3.4.1.min.js"></script>
 	<!-- Bootstrap JS -->
-	<script src="assets/front/js/bootstrap.min.js"></script>
+	<script src="public/assets/front/js/bootstrap.min.js"></script>
 	<!-- Aos Js Plugin-->
-	<script src="assets/front/js/aos.js"></script>
+	<script src="public/assets/front/js/aos.js"></script>
 	<!-- Jquery ui JS-->
-	<script src="assets/front/js/jquery-ui.js"></script>
+	<script src="public/assets/front/js/jquery-ui.js"></script>
 	<!--  Nav  -->
-	<script src="assets/front/js/jquery.smartmenus.js"></script>
+	<script src="public/assets/front/js/jquery.smartmenus.js"></script>
 
 	<!-- Revulotion Slider -->
-	<script src="assets/front/sliders/slider-1/js/revolution.tools.min.js"></script>
-	<script src="assets/front/sliders/slider-1/js/rs6.min.js"></script>
-	<script src="assets/front/sliders/slider-1/js/slideractivation.js"></script>
+	<script src="public/assets/front/sliders/slider-1/js/revolution.tools.min.js"></script>
+	<script src="public/assets/front/sliders/slider-1/js/rs6.min.js"></script>
+	<script src="public/assets/front/sliders/slider-1/js/slideractivation.js"></script>
 
 
 	<!--Owl carousel Slider-->
-	<script src="assets/front/js/owl.carousel.min.js"></script>
+	<script src="public/assets/front/js/owl.carousel.min.js"></script>
 	<!-- Main Counterdown Plugin-->
-	<script src="assets/front/js/countdown.js"></script>
+	<script src="public/assets/front/js/countdown.js"></script>
 	<!-- Fancybox Js-->
-	<script src="assets/front/js/jquery.fancybox.min.js"></script>
+	<script src="public/assets/front/js/jquery.fancybox.min.js"></script>
 	<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js'></script>
 	<!-- Main Script -->
-	<script src="assets/front/js/theme.js"></script>
+	<script src="public/assets/front/js/theme.js"></script>
--- resources/views/layouts/front_header.blade.php
+++ resources/views/layouts/front_header.blade.php
@@ -1,42 +1,42 @@
 <!--- Font-->
 <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700,800&amp;display=swap" rel="stylesheet">
-<link rel="stylesheet" href="assets/front/font/stylesheet.css">
+<link rel="stylesheet" href="public/assets/front/font/stylesheet.css">
 <link href="https://fonts.googleapis.com/css?family=Montserrat:800&amp;display=swap" rel="stylesheet">
 
 <!-- CSS -->
 <!-- Bootstrap CSS -->
-<link rel="stylesheet" href="assets/front/css/bootstrap.min.css">
+<link rel="stylesheet" href="public/assets/front/css/bootstrap.min.css">
 <!-- Jquery ui CSS -->
-<link rel="stylesheet" href="assets/front/css/jquery-ui.css">
+<link rel="stylesheet" href="public/assets/front/css/jquery-ui.css">
 <!-- Fancybox CSS -->
-<link rel="stylesheet" href="assets/front/css/jquery.fancybox.min.css">
+<link rel="stylesheet" href="public/assets/front/css/jquery.fancybox.min.css">
 
 <!-- Font Awesome CSS -->
-<link rel="stylesheet" href="assets/front/css/font-awosome.css">
+<link rel="stylesheet" href="public/assets/front/css/font-awosome.css">
 
 <!-- Flaticon CSS -->
-<link rel="stylesheet" href="assets/front/flat-font/flaticon.css">
+<link rel="stylesheet" href="public/assets/front/flat-font/flaticon.css">
 <!-- Ticker css-->
-<link rel="stylesheet" href="assets/front/css/ticker.min.css">
+<link rel="stylesheet" href="public/assets/front/css/ticker.min.css">
 <!--Owl carousel Slider -->
-<link rel="stylesheet" href="assets/front/css/owl.carousel.min.css">
-<link rel="stylesheet" href="assets/front/css/owl.theme.default.min.css">
+<link rel="stylesheet" href="public/assets/front/css/owl.carousel.min.css">
+<link rel="stylesheet" href="public/assets/front/css/owl.theme.default.min.css">
 <!-- Nav Menu CSS -->
-<link rel="stylesheet" href="assets/front/css/sm-core-css.css">
-<link rel="stylesheet" href="assets/front/css/sm-mint.css">
-<link rel="stylesheet" href="assets/front/css/sm-style.css">
+<link rel="stylesheet" href="public/assets/front/css/sm-core-css.css">
+<link rel="stylesheet" href="public/assets/front/css/sm-mint.css">
+<link rel="stylesheet" href="public/assets/front/css/sm-style.css">
 <!-- Animate CSS -->
-<link rel="stylesheet" href="assets/front/css/aos.css">
-<link rel="stylesheet" href="assets/front/css/animate.min.css">
+<link rel="stylesheet" href="public/assets/front/css/aos.css">
+<link rel="stylesheet" href="public/assets/front/css/animate.min.css">
 
 <!-- Revulation Slider -->
-<link rel="stylesheet" href="assets/front/sliders/slider-1/fonts/pe-icon-7-stroke/css/pe-icon-7-stroke.css">
-<link rel="stylesheet" href="assets/front/sliders/slider-1/fonts/font-awesome/css/font-awesome.css">
-<link rel="stylesheet" href="assets/front/sliders/slider-1/css/rs6.css">
+<link rel="stylesheet" href="public/assets/front/sliders/slider-1/fonts/pe-icon-7-stroke/css/pe-icon-7-stroke.css">
+<link rel="stylesheet" href="public/assets/front/sliders/slider-1/fonts/font-awesome/css/font-awesome.css">
+<link rel="stylesheet" href="public/assets/front/sliders/slider-1/css/rs6.css">
 <!-- Main StyleSheet CSS -->
-<link rel="stylesheet" href="assets/front/css/style.css">
+<link rel="stylesheet" href="public/assets/front/css/style.css">
 <!-- Favicon -->
-<link rel="icon" type="image/png" sizes="16x16" href="assets/front/img/logo.png">
+<link rel="icon" type="image/png" sizes="16x16" href="public/assets/front/img/logo.png">
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
@@ -45,7 +45,7 @@
 
 
  {{-- vue styles--}}
- <link href="{{ asset('css/front_app.css') }}" rel="stylesheet">
+ <link href="{{ asset('public/css/front_app.css') }}" rel="stylesheet">
 
 
  <!-- CSRF Token -->
